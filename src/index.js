23ff64ff2ed00d34831d206870960be1fd425453
griseldagomez <grinoe123@gmail.com>
2024-09-18 13:06:14 +0200

    update delete

--- src/index.js
+++ src/index.js
@@ -22,75 +22,100 @@ server.use(express.json());
 
 //insertar registro de usuario
 server.post("/user", async (req, res) => {
-  const connection = await getDBConnection();
-  const params = req.body;
-  const userQuerySQL =
-    "INSERT INTO users (user,name, email, password) VALUES(?,?,?, ?)";
-  const [userResult] = await connection.query(userQuerySQL, [
-    params.user,
-    params.name,
-    params.email,
-    params.password
-  ]);
-  connection.end();
-  res.status(201).json({
-    status: "success",
-    result: userResult,
-  });
-})
+
+  try {
+    const connection = await getDBConnection();
+    const params = req.body;
+    const userQuerySQL =
+      "INSERT INTO users (user,name, email, password) VALUES(?,?,?, ?)";
+    const [userResult] = await connection.query(userQuerySQL, [
+      params.user,
+      params.name,
+      params.email,
+      params.password
+    ]);
+    connection.end();
+    res.status(201).json({
+      status: "success",
+      result: userResult,
+    });
+  }
+  catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error"
+    })
+  }
+});
 
 
 //leer registros
 server.get("/user", async (req, res) => {
-  const connection = await getDBConnection();
-  const params = req.query;
-  const userQuerySQL =
-    "SELECT * FROM users";
-  const [userResult] = await connection.query(userQuerySQL, [
-    params.user,
-    params.name,
-    params.email,
-    params.password
-  ]);
-  connection.end();
-  res.status(201).json({
-    status: "success",
-    result: userResult,
-  });
-})
+
+  try {
+    const connection = await getDBConnection();
+    const params = req.query;
+    const userQuerySQL =
+      "SELECT * FROM users";
+    const [userResult] = await connection.query(userQuerySQL, [
+      params.user,
+      params.name,
+      params.email,
+      params.password
+    ]);
+    connection.end();
+    res.status(201).json({
+      status: "success",
+      result: userResult,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error"
+    })
+  }
+});
 
 
 //Actualizar datos del registro
 server.put("/user/:idUser", async (req, res) => {
-  const id = req.params.idUser; // url params
-  const connection = await getDBConnection();
-  const newData = req.body
-  const userQuerySQL =
-  "UPDATE users SET  user = ?, name = ?, email = ?, password = ? WHERE  idUser = ?";
-  const [userResult] = await connection.query(userQuerySQL, [
-    newData.user,
-    newData.name,
-    newData.email,
-    newData.password,
-    newData.idUser
-  ]);
-  connection.end();
 
-  res.status(201).json({
-    status: "success",
-    result: userResult,
-  });
+  try {
+    const connection = await getDBConnection();
+    const newData = req.body
+    const userQuerySQL =
+      "UPDATE users SET  user = ?, name = ?, email = ?, password = ? WHERE  idUser = ?";
+    const [userResult] = await connection.query(userQuerySQL, [
+      newData.user,
+      newData.name,
+      newData.email,
+      newData.password,
+      newData.idUser,
+    ]);
+    connection.end();
+
+    res.status(201).json({
+      status: "success",
+      result: userResult,
+    });
+  } catch
+  (err) {
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error"
+    })
+  }
 })
 
 //eliminar un registro
 server.delete("/user/:idUser", async (req, res) => {
-  const idUser = req.params.idUser;
+  const params = req.params.idUser;
   const connection = await getDBConnection();
   const sql = "DELETE from users WHERE idUser = ?";
   const [userResult] = await connection.query(sql, [params]
   );
   connection.end();
- 
+
   res.status(201).json({
     status: "success",
     result: userResult,
