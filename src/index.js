1dbd3066396b317e6e88661c450c496e8939ae87
griseldagomez <grinoe123@gmail.com>
2024-09-18 10:47:43 +0200

    endpoint insert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2/promise")
 
 // crear el servidor web
 const server = express();
 
+async function getDBConnection() {
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "admin",
+        database: "productos"
+    })
+    connection.connect();
+    return connection;
+}
+
 // necesito que mi servidor acepte peticiones externas
 server.use(cors());
+server.use(express.json());
 
-// establecer el puerto de conexión
+//insertar registro de usuario
+server.post("/user", async (req, res) => {
+  const connection = await getDBConnection();
+  const params = req.body;
+  const userQuerySQL =
+  "INSERT INTO users (user,name, email, password) VALUES(?,?,?, ?)";
+const [userResult] = await connection.query(userQuerySQL, [
+  params.user,
+  params.name,
+  params.email,
+  params.password
+]);
+connection.end();
+res.status(201).json({
+  status: "success",
+  result: userResult,
+});
+})
+
+
+//leer los registros
+
+server.
+
 const port = 5001;
 server.listen(port, () => {
   console.log("Server is running on port " + port);
